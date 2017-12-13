1754a431aa6eaa1a0042e8dfa26c5091ec4222ef
rvdabhi <rahul.dabhi19@gmail.com>
2017-12-13 19:40:55 +0530

    changes made

---
+++ starter-files/models/Store.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+	name:{
+		type: String, 
+		trim: true,
+		required: true
+	},
+	slug: String,
+	description: {
+		type: String,
+		trim: true
+	},
+	tags: [String]
+});
+
+storeSchema.pre('save', function(next) {
+	// body...
+
+	if(!this.isModified('name'))
+	{
+		next();
+		return;
+	}
+	this.slug = slug(this.name);
+	next();
+})
+module.exports = mongoose.model("Store", storeSchema);
